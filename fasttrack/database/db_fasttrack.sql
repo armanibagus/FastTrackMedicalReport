878eaeda9495fd290d371680a4887674d918a300
armanibagus <79289139+armanibagus@users.noreply.github.com>
2021-04-08 23:06:22 +0800

    Update db_fasttrack.sql
    Update the database

--- fasttrack/database/db_fasttrack.sql
+++ fasttrack/database/db_fasttrack.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 07 Apr 2021 pada 06.32
+-- Waktu pembuatan: 08 Apr 2021 pada 17.04
 -- Versi server: 10.4.17-MariaDB
 -- Versi PHP: 7.3.27
 
@@ -43,12 +43,8 @@ CREATE TABLE `center_officer` (
 --
 
 INSERT INTO `center_officer` (`officer_id`, `center_id`, `username`, `password`, `name`, `phone_number`, `address`, `position`) VALUES
-(1, 1, 'armanibagus', 'bagusarmani', 'Armani Bagus Saputra', '122312312', 'JL. Nangka, Denpasar, Bali, Indonesia', 'Manager'),
-(8, 1, 'tiwi', 'tiwitiwi', 'Dwi Candra Pertiwi', '12321', 'Br. Perang, Kapal, Badung', 'Tester'),
-(11, 1, 'kristi', 'kristi123', 'Kristi Imanuel', '3245678129', 'jalan kenangan', 'Tester'),
-(13, 1, 'bagas', 'bagas123', 'bagas saputra', '09312839173', 'Jl. nangka', 'Tester'),
-(16, 16, 'novita', 'novita123', 'Novita Indah', '01234556', 'di jumah ne', 'Manager'),
-(17, 0, 'niko', 'niko123', 'Niko', '089611234567', 'JL. Nangka, Denpasar, Bali', 'Manager');
+(1, 1, 'armanibagus', 'bagus123', 'Armani Bagus', '081235779653', 'Jl. Raya Puputan, No. 86, Kota Denpasar, Bali', 'Manager'),
+(2, 1, 'tiwi', 'tiwitiwi', 'Dwi Candra Pertiwi', '081765849329', 'JL. Perang, Badung, Bali', 'Tester');
 
 -- --------------------------------------------------------
 
@@ -72,7 +68,7 @@ CREATE TABLE `covid_test` (
 --
 
 INSERT INTO `covid_test` (`test_id`, `patient_id`, `officer_id`, `kit_id`, `test_date`, `result`, `result_date`, `status`) VALUES
-(10, 17, 8, 1, '6/April/2021', '', NULL, 'Pending');
+(1, 1, 2, 1, '8/April/2021', 'Positive Covid-19', '2021-04-08', 'Complete');
 
 -- --------------------------------------------------------
 
@@ -96,9 +92,7 @@ CREATE TABLE `patient` (
 --
 
 INSERT INTO `patient` (`patient_id`, `username`, `password`, `name`, `phone_number`, `address`, `patient_type`, `symptoms`) VALUES
-(1, 'diin', 'diin123', 'Adi Indrawan', '12321', 'jalan kenangan', 'Returnee', 'catch a chold'),
-(2, 'dasdas', 'dasda', 'dasda', '1212', 'dasda', 'Quarantined', 'dasda'),
-(17, 'aan', 'aan123', 'aan', '12345', 'Jl. nangka', 'Infected', 'gelem panes');
+(1, 'aan', 'aan123', 'Aan Nurjana', '089765345231', 'Jl. Kenanga, Denpasar, Bali', 'Infected', 'fever, headache, flu');
 
 -- --------------------------------------------------------
 
@@ -118,9 +112,7 @@ CREATE TABLE `test_center` (
 --
 
 INSERT INTO `test_center` (`center_id`, `center_name`, `phone_number`, `address`) VALUES
-(1, 'Puri Bunda', '1234567890', 'JL. Gatot Subroto, Denpasar, Bali, Indonesia'),
-(15, 'maharani', '234567890', 'denpasar'),
-(16, 'Sanglah', '1234567890', 'Jl. Diponegoro, Denpasar, Bali');
+(1, 'Puri Bunda', '036168754311', 'JL. Gatot Subroto, Denpasar, Bali, Indonesia');
 
 -- --------------------------------------------------------
 
@@ -141,9 +133,7 @@ CREATE TABLE `test_kit` (
 --
 
 INSERT INTO `test_kit` (`kit_id`, `center_id`, `name`, `stock`, `description`) VALUES
-(1, 1, 'Sinofac', 9998, 'Test kit from China'),
-(2, 1, 'Nasional', 1000, 'Test kit from Indonesia'),
-(5, 1, 'Rapid', 1000, 'Rapid test kit stock');
+(1, 1, 'Rapid', 199, 'Covid-19 test kit using Rapid test kit');
 
 --
 -- Indexes for dumped tables
@@ -188,31 +178,31 @@ ALTER TABLE `test_kit`
 -- AUTO_INCREMENT untuk tabel `center_officer`
 --
 ALTER TABLE `center_officer`
-  MODIFY `officer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `officer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT untuk tabel `covid_test`
 --
 ALTER TABLE `covid_test`
-  MODIFY `test_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `test_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `patient`
 --
 ALTER TABLE `patient`
-  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `patient_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `test_center`
 --
 ALTER TABLE `test_center`
-  MODIFY `center_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `center_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `test_kit`
 --
 ALTER TABLE `test_kit`
-  MODIFY `kit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `kit_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
